dae1354bf1824593ec1bb7cf42e2b04bd370fe3e
AbdullahAlTowsif <abdullah.al.towsif2002@gmail.com>
2024-09-20 02:22:33 +0600

    mfs done simply

--- js/addMoney.js
+++ js/addMoney.js
@@ -3,11 +3,21 @@ document.getElementById('btn-add-money').addEventListener('click', function(even
     const addMoney = getInputFieldValueById('input-add-money');
     const pinNumber = getInputFieldValueById('input-pin-number');
 
+    if(isNaN(addMoney)){
+        alert('Failed to add money');
+        return;
+    }
+
     if(pinNumber === 1234){
         const balance = getTextFieldValueById('main-balance');
         const newBalance = addMoney + balance;
 
         document.getElementById('main-balance').innerText = newBalance;
+        // add to transaction history
+        const entry = document.createElement('p');
+        entry.innerText = `${addMoney} added. Tk. Balance: ${newBalance}`;
+        // should be a common function
+        document.getElementById('trans-container').appendChild(entry);
     }
     else{
         alert('Failed to add the money!');
--- js/cashout.js
+++ js/cashout.js
@@ -3,10 +3,28 @@ document.getElementById('btn-cashout').addEventListener('click', function(event)
     const cashout = getInputFieldValueById('input-cashout');
     const pinNumber = getInputFieldValueById('cashout-pin-number');
 
+    if(isNaN(cashout)){
+        alert("invalid number");
+        return;
+    }
+
     if(pinNumber === 1234){
         const mainBalance = getTextFieldValueById('main-balance');
+        if(cashout > mainBalance){
+            alert('Insufficient Money');
+            return;
+        }
         const updatedBalance = mainBalance - cashout;
         document.getElementById('main-balance').innerText = updatedBalance;
+        // add to transaction history
+        const div = document.createElement('div');
+        div.classList.add('bg-yellow-300');
+        div.innerHTML = `
+        <h4 class="text-2xl font-bold">Cash Out</h4>
+        <p>${cashout} Withdraw. New Balance: ${updatedBalance} </p>
+        `
+
+        document.getElementById('trans-section').appendChild(div);
     }
     else{
         alert('No Money for you...DGM');
